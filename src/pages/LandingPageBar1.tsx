8f8ce4cda7a8b62ef1b82b98124bf46ce68f87b4
rajputshashank003 <hariomshashank@gmail.com>
2025-04-01 12:29:07 +0530

    change positions of landing page bar 1

--- src/pages/LandingPageBar1.tsx
+++ src/pages/LandingPageBar1.tsx
@@ -2,7 +2,7 @@ import BlurText from "../components/BlurText"
 
 const LandingPageBar1 = () => {
   return (
-    <div data-scroll data-scroll-speed="0.1" className='flex flex-col md:grid bg-[#FAF4F0] rounded-3xl p-8 m-8 -mt-40 md:mt-8 grid-cols-3 '>
+    <div data-scroll data-scroll-speed="0.1" className='flex flex-col md:grid bg-[#FAF4F0] rounded-3xl p-8 m-8 -mt-10 md:mt-8 grid-cols-3 '>
         <div className="flex justify-center items-center max-md:border-b-2 md:border-r-2 border-black pb-4">
             <img src="/small_img1.webp" className="rounded-full border-2 border-black"  alt="" />
         </div>
